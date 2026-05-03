11b39c3dbc8fcc9002456be115e1a06bddcd1267
mdashraful24 <mdashrafulislam2882@gmail.com>
2026-05-03 18:45:23 +0600

    explore and understood conditional types

---
+++ src/conditionalType.ts
@@ -0,0 +1,20 @@
+// ! Explore conditional type
+
+
+// * conditional type: type depends on condition
+
+type A = null;
+type B = undefined;
+
+type C = A extends number ? true : B extends undefined ? true : false;
+
+
+type RichPeopleVehicle = {
+    bike: string;
+    car: string;
+    ship: string;
+};
+
+type CheckVehicle<T> = T extends keyof RichPeopleVehicle ? true : false;
+
+type HasBike = CheckVehicle<"bike">;
